200d9098eb06285de06766d63f9888983dc19224
DESKTOP-OVFV7BQ\SK <bhagwan@Shopkirana.com>
2024-06-14 11:50:16 +0530

    Chnages in  web view

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,14 @@ import 'package:direct_sourcing_agent/view/profile_type/ProfileTypes.dart';
 import 'package:direct_sourcing_agent/view/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permission.camera.request();
+  await Permission.microphone.request();
   runApp( MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => DataProvider()),
--- lib/view/dashboard/leadcreate/CreateLeadWebView.dart
+++ lib/view/dashboard/leadcreate/CreateLeadWebView.dart
@@ -1,11 +1,21 @@
 import 'dart:io';
 
+import 'package:direct_sourcing_agent/providers/DataProvider.dart';
 import 'package:direct_sourcing_agent/shared_preferences/shared_pref.dart';
 import 'package:direct_sourcing_agent/utils/ImagePicker.dart';
 import 'package:direct_sourcing_agent/utils/constant.dart';
+import 'package:direct_sourcing_agent/utils/utils_class.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
@@ -18,9 +28,9 @@ class WebViewExample extends StatefulWidget {
 
   WebViewExample(
       {required this.mobileNumber,
-      required this.companyID,
-      required this.productID,
-      required this.token});
+        required this.companyID,
+        required this.productID,
+        required this.token});
 
   @override
   State<WebViewExample> createState() => _WebViewExampleState();
@@ -32,24 +42,106 @@ class _WebViewExampleState extends State<WebViewExample> {
   int? productID;
   bool checkCamera = false;
   bool openCameraOnly = false;
-  late String _cameraPhotoPath="";
+  late String _cameraPhotoPath;
   final ImagePicker _picker = ImagePicker();
+
+  final browser = MyInAppBrowser();
+
+
+
+  final settings = InAppBrowserClassSettings(
+      browserSettings: InAppBrowserSettings(hideUrlBar: true),
+      webViewSettings: InAppWebViewSettings(
+          javaScriptEnabled: true, isInspectable: kDebugMode));
+
 
   @override
   void initState() {
     super.initState();
-    late final PlatformWebViewControllerCreationParams params;
 
+
+
+    openInAppBrowser(widget.token!);
+
+    /* late final PlatformWebViewControllerCreationParams params;
     if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(allowsInlineMediaPlayback: true, mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},);
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
     } else {
       params = const PlatformWebViewControllerCreationParams();
     }
 
-    final WebViewController controller = WebViewController.fromPlatformCreationParams(params, onPermissionRequest: (resources) async {
+    final WebViewController controller = WebViewController.fromPlatformCreationParams(params);
+     _controller = WebViewController.fromPlatformCreationParams(params, onPermissionRequest: (resources) async {
+      return resources.grant();
+    });
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+            _controller.runJavaScript("_callJavaScriptFunction('${widget.token}')");
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+          onHttpError: (HttpResponseError error) {
+            debugPrint('Error occurred on page: ${error.response?.statusCode}');
+          },
+          onUrlChange: (UrlChange change) {
+            debugPrint('url change to ${change.url}');
+          },
+          onHttpAuthRequest: (HttpAuthRequest request) {},
+        )
+      )
+      ..addJavaScriptChannel(
+        '"cameraPermission"',
+        onMessageReceived: (JavaScriptMessage message) {
+          print("Call Method$message");
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse(_constructUrl()));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;*/
+
+    /* late final PlatformWebViewControllerCreationParams params;
+
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(
+      params,
+      onPermissionRequest: (resources) async {
         return resources.grant();
       },
-
     );
 
 
@@ -66,18 +158,17 @@ class _WebViewExampleState extends State<WebViewExample> {
           },
           onPageFinished: (String url) {
             debugPrint('Page finished loading: $url');
-            controller.runJavaScript("_callJavaScriptFunction('${widget.token}')");
-           // controller.runJavaScript("getDocUrlFromFlutter('${_cameraPhotoPath}')");
-
-            /* controller.runJavaScript(
+            controller
+                .runJavaScript("_callJavaScriptFunction('${widget.token}')");
+            // controller.runJavaScript(
               'openCameraOnly.postMessage("User Agent: " + navigator.userAgent);',
-            );*/
+            );*//*
 
-          /*  controller.runJavaScript('''
+          //  controller.runJavaScript('''
       document.addEventListener('click', (event) => {
      openCameraOnly.postMessage("false");
       });
-    ''');*/
+    ''');*//*
           },
           onNavigationRequest: (NavigationRequest request) {
             return NavigationDecision.navigate;
@@ -94,10 +185,10 @@ class _WebViewExampleState extends State<WebViewExample> {
         ),
       )
 
-      /*..addJavaScriptChannel(
-        'openFlutterCameraOnly',
+      ..addJavaScriptChannel(
+        'openCameraOnly',
         onMessageReceived: (JavaScriptMessage message) {
-         *//* ScaffoldMessenger.of(context).showSnackBar(
+          ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text(message.message)),
           );
           bool enable = message.message == 'true';
@@ -108,27 +199,43 @@ class _WebViewExampleState extends State<WebViewExample> {
             _showFileChooser();
           } else {
             bottomSheetMenu(context);
-          }*//*
-        },)*/
+          }
+        },
+      )
       ..loadRequest(Uri.parse(_constructUrl()));
+
+// #docregion platform_features
     if (controller.platform is AndroidWebViewController) {
       AndroidWebViewController.enableDebugging(true);
-      (controller.platform as AndroidWebViewController).setMediaPlaybackRequiresUserGesture(false);
-      (controller.platform as AndroidWebViewController).clearCache();
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
     }
-    _controller = controller;
+// #enddocregion platform_features
+
+    _controller = controller;*/
   }
+
+  void openInAppBrowser(String token) {
+    browser.token=token;
+    browser.openUrlRequest(
+      urlRequest: URLRequest(url: WebUri(_constructUrl())),
+      settings: settings,
+
+    );
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('Create Lead'),
-          // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
-        ),
-        body: WebViewWidget(controller: _controller),
-       /* floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Create Lead'),
+        // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
+      ),
+      body:Container(),
+      /* floatingActionButton: FloatingActionButton(
           onPressed: () {
             _controller.runJavaScript(
                 "cameraPermission('${bottomSheetMenu(context)}')");
@@ -136,7 +243,7 @@ class _WebViewExampleState extends State<WebViewExample> {
         )*/);
   }
 
-  bottomSheetMenu(BuildContext context) {
+  /*bottomSheetMenu(BuildContext context) {
     showModalBottomSheet(
         context: context,
         builder: (builder) {
@@ -145,14 +252,14 @@ class _WebViewExampleState extends State<WebViewExample> {
   }
 
   void _onImageSelected(File imageFile) async {
-    _cameraPhotoPath =imageFile.path;
     // Handle the selected image here
     // For example, you can setState to update UI with the selected image;
-    /*Utils.onLoading(context, "");
-    await Provider.of<DataProvider>(context, listen: false).postSingleFile(imageFile, true, "", "");
+    Utils.onLoading(context, "");
+    await Provider.of<DataProvider>(context, listen: false)
+        .postSingleFile(imageFile, true, "", "");
     // Navigator.pop(context);
-    Navigator.of(context, rootNavigator: true).pop();*/
-  }
+    Navigator.of(context, rootNavigator: true).pop();
+  }*/
 
   String _constructUrl() {
     String baseUrl = "https://customer-qa.scaleupfin.com/#/lead";
@@ -168,31 +275,39 @@ class _WebViewExampleState extends State<WebViewExample> {
     productID = prefsUtil.getInt(PRODUCT_ID);
   }
 
-  Future<File> createImageFile() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final path = directory.path;
-    final file = File('$path/temp_photo.jpg');
-    return file;
+
+
+}
+
+class MyInAppBrowser extends InAppBrowser {
+  var token;
+
+
+  MyInAppBrowser();
+  @override
+  Future onBrowserCreated() async {
+
   }
 
-  Future<void> _showFileChooser() async {
-    if (openCameraOnly) {
-      final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
-      if (photo != null) {
-        setState(() {
-          _cameraPhotoPath = photo.path;
-        });
-      }
-    } else {
-      // You can add more functionality here to handle other file types if needed
-      final XFile? file = await _picker.pickImage(source: ImageSource.gallery);
-      if (file != null) {
-        setState(() {
-          _cameraPhotoPath = file.path;
-          print("ImagePath"+_cameraPhotoPath);
-        });
-      }
-    }
+  @override
+  Future onLoadStart(url) async {
+    print("Started $url");
+  }
+
+  @override
+  Future onLoadStop(url) async {
+    print("Stopped $token");
+    await webViewController?.evaluateJavascript(source: "_callJavaScriptFunction('${token}')");
+  }
+
+  @override
+  void onProgressChanged(progress) {
+    print("Progress: $progress");
+  }
+
+  @override
+  void onExit() {
+    print("Browser closed!");
   }
 }
 
